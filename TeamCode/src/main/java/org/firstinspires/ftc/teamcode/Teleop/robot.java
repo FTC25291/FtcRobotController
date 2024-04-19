e6fd5d61001b01788c4254319c9deabc0fd142dc
Timothy Mitchell <timothy.mitchell2022@gmail.com>
2024-04-19 17:12:47 -0400

    Update robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/robot.java
@@ -1,1 +1,51 @@
-import robot 
+/*
+Imports
+
+creation class
+
+when button pressed
+    activate process
+
+
+Driver Joysticks
+    Call the drive.java
+        - Current Joystick Values
+
+Drive A Button
+
+Driver B Button
+    PickUp
+        - Arm angle = 60 degrees
+        - Claw angle 90 degrees
+        - Extend to set position & let RT and LT extend and retract the arm
+        - Turn on the corresponding LED light
+            - Blue
+
+Driver Y Button
+    Stow
+        - Arm angle = 90 degrees
+        - Claw angle = 180 degrees
+        - Fully retracted
+        - Turn on the corresponding LED light
+            - Flashing Yellow
+
+Driver X Button
+    Placing
+        - Arm angle = 230 degrees
+        - Claw angle = 180 degrees
+        - Extend arm angle to set position & let RT and LT extend and retract the arm
+        - Extend claw angle to set position & let RB and LB extend and retract the arm
+            - Green
+Driver LT
+    Extending Arm
+
+Driver RT
+    Retracting Arm
+
+Driver LB
+    Open Claw
+
+Driver RB
+    Close Claw
+
+ */
