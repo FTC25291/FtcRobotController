4cf90b5a36c0abaff02b8c720b1abb7ca3499006
CodeWizDurham <durham@thecodewiz.com>
2024-04-20 14:49:32 -0700

    Update Arm.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Arm.java
@@ -25,37 +25,43 @@ Close Claw - Claw class button input function
 //Imports for the motors
 
 public class Arm {
+    private RevArmAngleMoter armAngle;
+    private RobotArm robotArm;
+    private ExtendMotor extendMotor;
+    private Gamepad gamepad1;
 
-    if (gamepad1.y == true) {
-        // Code to move into stowed position
-        // Lock
-        robotArm.stow();
+    public Arm(RevArmAngleMoter armAngle, RobotArm robotArm, ExtendMotor extendMotor, Gamepad gamepad1) {
+        this.armAngle = armAngle;
+        this.robotArm = robotArm;
+        this.extendMotor = extendMotor;
+        this.gamepad1 = gamepad1;
     }
 
-    if (gamepad1.b == true) {
-        // Code to move into placing position
-        // Unlock
-        robotArm.place();
+    public void controlArm() {
+        if (gamepad1.y) {
+            armAngle.setAngle(RevArmAngleMoter.ArmAngle.degrees90);
+            robotArm.stow();
+        }
+
+        if (gamepad1.b) {
+            armAngle.setAngle(RevArmAngleMoter.ArmAngle.degrees230);
+            robotArm.place();
+        }
+
+        if (gamepad1.x) {
+            armAngle.setAngle(RevArmAngleMoter.ArmAngle.degrees60);
+            robotArm.pick();
+        }
+
+        // Code for the extension and retraction of the telescoping arm
+        if (gamepad1.right_trigger > 0) {
+            extendMotor.setPower(1);
+            robotArm.extend();
+        }
+
+        if (gamepad1.left_trigger > 0) {
+            extendMotor.setPower(-1);
+            robotArm.retract();
+        }
     }
-
-    if (gamepad1.x == true) {
-        // Code for moving into picking up position
-        // Unlock
-        robotArm.pick();
-    }
-
-    // Code for the extension and retraction of the telescoping arm
-    while (gamepad1.right_trigger == true) {
-        ExtendMotor.setPower(1);
-        robotArm.extend();
-    }
-
-    while (gamepad1.left_trigger == true) {
-        ExtendMotor.setPower(-1);
-        robotArm.retract();
-    }
-}
-
-
-
 }
