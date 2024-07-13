71eac4c0749e69fa9ddbd0218313b4bef601b8b8
CodeWizDurham <durham@thecodewiz.com>
2024-07-13 15:23:38 -0400

    auto updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Autonomous.java
@@ -27,7 +27,12 @@ public class Autonomous extends LinearOpMode {
 //5 inch acceleration for forward so subtract 5 from desired location
 //4 inch acceleration for backward so subtract 4 from desired location
         // When going 12 inches, went 8.25 inches
-        autodrive.update_Drive("left", 17.5);
+        autodrive.update_Drive("fontrightstrafe", (15 + math.sqrt(2)));
+        autodrive.update_Drive("backleftstrafe", (15 + math.sqrt(2)));
+        autodrive.update_Drive("forward", 15);
+        autodrive.update_Drive("turnleft90", 3);
+        
+        autodrive.update_Drive("turnleft90", 23*4)
 
 
     }
